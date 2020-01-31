e9cabfac5b1d4a3853c85534a4e06d6e191d4bcf
Tatiane Aguirres Nogueira <aguirres.tati@gmail.com>
2020-01-31 19:17:24 +0100

    :recycle: Refactor the <App/> component to receive new components

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Home from '../components/home/home'
 import Header from '../components/header/header'
+import Footer from '../components/footer/footer'
+import { headerData, footerData } from '../mocks/works-data'
 
 function App() {
   return (
     <div className='App'>
-      <Header />
+      <Header headerData={headerData} />
       <Home />
+      <Footer footerData={footerData} />
     </div>
   )
 }
