77fab87aa8d368237e53fabb098a391bf771303d
Tatiane Aguirres Nogueira <aguirres.tati@gmail.com>
2020-01-31 19:17:01 +0100

    :recycle: Refactor the <Home/> component to receive new components

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Works from '../works/works'
+import Clients from '../clients/clients'
+import Form from '../form/form'
+import { clientsDescription, clientsData } from '../../mocks/clients-data'
+import { testemonialData, worksData } from '../../mocks/works-data'
 
 const Home = () => {
   return (
     <div>
-      <h1>Dept Agency</h1>
+      <Works worksData={worksData} testemonialData={testemonialData} />
+      <Clients
+        clientsDescription={clientsDescription}
+        clientsData={clientsData}
+      />
+      <Form />
     </div>
   )
 }
